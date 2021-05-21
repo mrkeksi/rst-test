e167165c8c7ebd57fd96650f4b0f9eede4b759db
MrKeksi <x.treme.field@gmx.de>
2021-05-21 23:12:49 +0200

    uz

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `rst IT-Unternehmensberatung`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@rst`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
